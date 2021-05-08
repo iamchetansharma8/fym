8b67241cbd9d774e0489ec2ad33f35513ef6bd7f
Chetan Sharma <iamchetansharma8@gmail.com>
2021-05-08 21:04:26 +0530

    added searching facility for user in home, minor style changes are made and maxlength property is added to post title and post description

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,38 @@
 const User=require('../models/user');
 
 module.exports.home=function(req,res){
-    User.find({},function(err,users){
-        return res.render('home',{
-            title:'Home',
-            all_users:users
+    let noMatch = null,searched=false;
+    if(req.query.search){
+        searched=true;
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        User.find({name:regex},function(err,users){
+            if(err){
+                console.log(err);
+            }else{
+                if(users.length < 1) {
+                    noMatch = "No user matched that query, please try again.";
+                }
+                return res.render('home',{
+                    title:'Find User',
+                    all_users:users,
+                    noMatch: noMatch,
+                    searched:searched
+                });
+            }
         });
-    });
+    }
+    else{
+        User.find({},function(err,users){
+            return res.render('home',{
+                title:'Home',
+                all_users:users,
+                noMatch: noMatch,
+                searched:searched
+            });
+        });
+    }
 }
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
