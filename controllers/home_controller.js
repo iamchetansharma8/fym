a2ee34cc1dd91c83000afded3313f9d922f8f212
Chetan Sharma <iamchetansharma8@gmail.com>
2021-05-09 08:52:25 +0530

    added feature to search a user by his topic of interest

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,12 @@
 const User=require('../models/user');
+const Topic=require('../models/topic');
 
 module.exports.home=function(req,res){
-    let noMatch = null,searched=false;
-    if(req.query.search){
+    let noMatch = null,searched=false,by_name=false,by_topic=false;
+    if(req.query.namesearch){
         searched=true;
-        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        by_name=true;
+        const regex = new RegExp(escapeRegex(req.query.namesearch), 'gi');
         User.find({name:regex},function(err,users){
             if(err){
                 console.log(err);
@@ -16,7 +18,31 @@ module.exports.home=function(req,res){
                     title:'Find User',
                     all_users:users,
                     noMatch: noMatch,
-                    searched:searched
+                    searched:searched,
+                    by_name:by_name,
+                    by_topic:by_topic
+                });
+            }
+        });
+    }
+    else if(req.query.topicsearch){
+        searched=true;
+        by_topic=true;
+        const regex = new RegExp(escapeRegex(req.query.topicsearch), 'gi');
+        Topic.find({name:regex}).populate({path:'users'}).exec(function(err,topics){
+            if(err){
+                console.log(err);
+            }else{
+                if(topics.length < 1) {
+                    noMatch = "Topic does'nt exist as of now, you can create it in you profile section";
+                }
+                return res.render('home',{
+                    title:'Find User',
+                    all_topics:topics,
+                    noMatch: noMatch,
+                    searched:searched,
+                    by_name:by_name,
+                    by_topic:by_topic
                 });
             }
         });
